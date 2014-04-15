ff116b55b6722ea95c2d5d11642d7f3a249a2699
Justin Li <jli.justinli@gmail.com>
2014-04-14 20:00:14 -0400

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    name='osxmpdkeys',
+    version='0.0.1',
+    description='Control mpd with the OS X media keys.',
+    long_description=open('README.rst').read(),
+    author='Justin Li',
+    author_email='jli@j-li.net',
+    url='https://github.com/pushrax/osxmpdkeys',
+    license='MIT',
+    packages=[
+        'osxmpdkeys'
+    ],
+    scripts=[
+        'mpdkeys'
+    ],
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: MacOS X',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+    ]
+)
+
